d96c648531c36c87839882584f9374787f92ef03
Kataplan <dcatalanmarchese@gmail.com>
2022-11-05 19:34:54 -0300

    Change name of Normalize.py to functions_sv.py, add comments, fix normalize

--- functions_sv.py
+++ functions_sv.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-
-
 def readFile(string):
     file_data = np.genfromtxt(string, dtype=str, delimiter=",")
     return file_data
 
+#function to reduce the data given the config limiters
 def reduce_data(file_data, data_limit, class_1_bool,class_2_bool, class_3_bool):
     new_file_data=[]
     row=0
@@ -13,7 +12,8 @@ def reduce_data(file_data, data_limit, class_1_bool,class_2_bool, class_3_bool):
     data_class_2= 0
     data_class_3= 0
     for i in file_data[:, 41]:
-        i=int(i)
+        i=int(i) #parse i to match condition
+        #condition for the first row added to new_file_data
         if(data_class_1+data_class_2+data_class_3 == 0):
             if(class_1_bool and i == 1):
                 new_file_data = [file_data[row]]
@@ -27,6 +27,7 @@ def reduce_data(file_data, data_limit, class_1_bool,class_2_bool, class_3_bool):
                 new_file_data = [file_data[row]] 
                 data_class_3= data_class_3 + 1
         
+        #check if it available class, if is the number and if bellow the data limit
         elif(class_1_bool and i == 1 and data_class_1 < data_limit):
             new_file_data = np.append(new_file_data, [file_data[row]], axis=0)
             data_class_1= data_class_1 + 1
@@ -44,11 +45,12 @@ def reduce_data(file_data, data_limit, class_1_bool,class_2_bool, class_3_bool):
 
 def classify(file_data):
     count = 0
+    #data dictionary
     class_1 = ['normal']
     class_2 = ['neptune', 'teardrop', 'smurf', 'pod', 'back', 'land', 'apache2', 'processtable', 'mailbomb', 'udpstorm']
     class_3 = ['ipsweep', 'portsweep', 'nmap', 'satan', 'saint', 'mscan']
+    #cycling for each data in the column 41 to change the value from de data dictionary to number value
     for i in file_data[:, 41]:
-    
         if i in class_1:
             file_data[count, 41] = 1    
             
@@ -58,6 +60,7 @@ def classify(file_data):
         elif i in class_3:
             file_data[count, 41] = 3
         else:
+            #if not in de data dictionary delete the complete row
             file_data = np.delete(file_data, count, axis=0)
             count = count - 1
 
@@ -65,6 +68,7 @@ def classify(file_data):
     
     return file_data
 
+#find the array with only one of each data in the column
 def singularity_array(file_data, column):
     array = []
     for i in file_data[:, column]:
@@ -73,6 +77,7 @@ def singularity_array(file_data, column):
 
     return array
 
+#function to change from categoric variables to numeric
 def transform_categoric_variables(file_data, column):
     array = singularity_array(file_data, column)
     count = 0
@@ -81,11 +86,14 @@ def transform_categoric_variables(file_data, column):
         count = count +1
     return file_data
 
+#change the matrix to integer
 def matrix_to_integer(X):
     for i in range(len(X)):
         for j in range(len(X[0])):
             X[i][j] = float(X[i][j])
     return X
+
+#function to find the maximun in column
 def findMax(file_data, column):
     max = 0
     for i in file_data[:, column]:
@@ -93,6 +101,7 @@ def findMax(file_data, column):
             max = float(i)
     return max
 
+#function to find the minimun in column
 def findMin(file_data, column):
     min = 900000
     for i in file_data[:, column]:
@@ -100,24 +109,25 @@ def findMin(file_data, column):
             min = float(i)
     return min
 
+#Normalize one column
 def normalize_column(file_data, column):
     min = findMin(file_data, column)
     max = findMax(file_data, column)
     b = 0.99
     a = 0.01
     row=0
-    for x in file_data[:,column]:
-        
-        file_data[row,column]= float(file_data[row,column])
-        x= float(x)
-        file_data[row,column] = ( ( x - min ) / ( max - min )) * ( b - a ) + a 
-        row = row + 1
+    if( max != min ):
+        for x in file_data[:,column]:
+            
+            file_data[row,column]= float(file_data[row,column])
+            x= float(x)
+            file_data[row,column] = ( ( x - min ) / ( max - min )) * ( b - a ) + a 
+            row = row + 1
     return file_data
 
+#Normalize all the data
 def normalize_data(file_data):
     for i in range(0 , 40):
-        if(i == 8):
-            return file_data
         file_data = normalize_column(file_data,i)
     return file_data
 
