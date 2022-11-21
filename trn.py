ce0ebbffe7785bdc8ca7a298b2dea57ff30179c5
Kataplan <dcatalanmarchese@gmail.com>
2022-11-21 11:47:31 -0300

    Update trn

--- trn.py
+++ trn.py
@@ -20,27 +20,32 @@ def save_w():
     return
 
 # Training: ANN-BP
-def ann_bp(w1,w2,x,y):
-    ...    
+def ann_bp(w1,w2,x,y, param):
+    iteration_number = param[0]
+    learning_rate = param[1]
     return(w1,w2)
 
 # Training : ANN-PSO
-def ann_pso(x,y,particles_amount):
+def ann_pso(x,y,param):
+    hidden_activation = param[0]
+    hidden_nodes_number = param[1]
+    particle_number = param[2]
+    iteration_number = param[3]
+    space = pso.iniSwarm(iteration_number,particle_number)
     
-
-
     return(w1,w2)
 # Training:ANN-PSO and  ANN-BP
-def train_ann(x,y,param):
-    w1,w2 = ann_pso(x,y,...)
-    w1,w2 = ann_bp(w1,w2,x,y,...)    
+def train_ann(x,y,param_PSO,param_bp):
+    w1,w2 = ann_pso(x,y,param_PSO)
+    w1,w2 = ann_bp(w1,w2,x,y,param_bp)    
     return(w1,w2) 
    
 # Beginning ...
 def main():
-    param   = load_config()            
-    xe,ye   = load_data()   
-    w1,w2   = train_ann(xe,ye,param)             
+    param_PSO = load_config("cnf_ann_pso.csv")            
+    param_bp = load_config("cnf_ann_bp.csv")            
+    xe,ye = load_data()   
+    w1,w2 = train_ann(xe,ye,param_PSO,param_bp)             
     save_w(w1,w2)
        
 if __name__ == '__main__':   
--- util_pso.py
+++ util_pso.py
@@ -2,24 +2,47 @@
 
 import numpy    as np
 import util_bp  as np
+import random 
 
-# class Particle:
-#     position_i = []
-#     velocity_i = []
-#     pos_best_i = []
-#     err_best_i =-1
-#     err_i = -1
-#     def __init__(self, position_i, velocity_i, pos_best_i, err_best_i, err_i):
-#         self.position_i = position_i
-#         self.velocity_i = velocity_i
-#         self.pos_best_i = pos_best_i
-#         self.err_best_i = err_best_i
-#         self.err_i = err_i
-       
+class Particle:
+   
+    def __init__(self):
+        self.position = np.array([(-1) ** (bool(random.getrandbits(1))) * random.random()*50, (-1)**(bool(random.getrandbits(1))) * random.random()*50])
+        self.pbest_position = self.position
+        self.pbest_value = float('inf')
+        self.velocity = np.array([0,0])
+    def move(self):
+        self.position_i = self.position_i + self.velocity_i
+
+class Space():
+
+    def __init__(self, n_particles,particles):
+        self.n_particles = n_particles
+        self.particles = particles
+        self.gbest_value = float('inf')
+        self.gbest_position = np.array([random.random()*50, random.random()*50])
+    
+    def set_pbest(self):
+        for particle in self.particles:
+            fitness_cadidate = self.fitness(particle)
+            if(particle.pbest_value > fitness_cadidate):
+                particle.pbest_value = fitness_cadidate
+                particle.pbest_position = particle.position
+            
+
+    def set_gbest(self):
+        for particle in self.particles:
+            best_fitness_cadidate = self.fitness(particle)
+            if(self.gbest_value > best_fitness_cadidate):
+                self.gbest_value = best_fitness_cadidate
+                self.gbest_position = particle.position
+
+    
 #Swarm: Initializing
-def iniSwarm():
-    ...
-    return()
+def iniSwarm(particle_number):
+    particles_vector = [Particle() for _ in range(particle_number)]
+    space = Space.__init__(space,particle_number, particles_vector)
+    return(space)
 # Fitness by use MSE
 def Fitness_mse():
     ...
@@ -30,7 +53,7 @@ def upd_pFitness():
     ...
     return()
 # Update: Swarm's velocity
-def upd_veloc():
+def upd_veloc(particle:Particle,pos_best_g):
     ...
     return()  
   
