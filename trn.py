cd7e08b1e109d190b33722b019571d433829c89e
Kataplan <dcatalanmarchese@gmail.com>
2022-11-21 10:01:33 -0300

    Update trn.py

--- trn.py
+++ trn.py
@@ -6,26 +6,29 @@ import util_pso as pso
 import util_bp  as bp
 
 # Load Parameters
-def load_config():
-    ...
-    return(param)
+def load_config(file_name:str):
+    file_data = np.genfromtxt(file_name, dtype=float, delimiter=",")
+    return(file_data)
 # Load data of training
-def load_data():
-	...
-	return(x,y)
+def load_data(x_name,y_name):
+    x = np.genfromtxt(x_name, dtype=float, delimiter=",")
+    y = np.genfromtxt(y_name, dtype=float, delimiter=",")
+    return(x,y)
 #save weights in numpy format
 def save_w():
     ...
     return
 
 # Training: ANN-BP
-def ann_bp(w1,w2,x,y,..):
+def ann_bp(w1,w2,x,y):
     ...    
     return(w1,w2)
 
 # Training : ANN-PSO
-def ann_pso(x,y,...):
-    ...    
+def ann_pso(x,y,particles_amount):
+    
+
+
     return(w1,w2)
 # Training:ANN-PSO and  ANN-BP
 def train_ann(x,y,param):
@@ -37,7 +40,7 @@ def train_ann(x,y,param):
 def main():
     param   = load_config()            
     xe,ye   = load_data()   
-    w1,w2   = ann_train(xe,ye,param)             
+    w1,w2   = train_ann(xe,ye,param)             
     save_w(w1,w2)
        
 if __name__ == '__main__':   
