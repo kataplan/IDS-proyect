54f78bdae81f5031ca5ae9e4693002ab9af4be1d
Kataplan <dcatalanmarchese@gmail.com>
2022-11-21 10:01:58 -0300

    Update util_pso.py

--- util_pso.py
+++ util_pso.py
@@ -3,6 +3,19 @@
 import numpy    as np
 import util_bp  as np
 
+# class Particle:
+#     position_i = []
+#     velocity_i = []
+#     pos_best_i = []
+#     err_best_i =-1
+#     err_i = -1
+#     def __init__(self, position_i, velocity_i, pos_best_i, err_best_i, err_i):
+#         self.position_i = position_i
+#         self.velocity_i = velocity_i
+#         self.pos_best_i = pos_best_i
+#         self.err_best_i = err_best_i
+#         self.err_i = err_i
+       
 #Swarm: Initializing
 def iniSwarm():
     ...
