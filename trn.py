7b30f3100ec406f7d67347a9a6c53959b1614eb3
Kataplan <dcatalanmarchese@gmail.com>
2022-11-24 22:41:12 -0300

    Update trn.py

--- trn.py
+++ trn.py
@@ -7,7 +7,7 @@ import util_bp  as bp
 
 # Load Parameters
 def load_config(file_name:str):
-    file_data = np.genfromtxt(file_name, dtype=float, delimiter=",")
+    file_data = np.genfromtxt(file_name, dtype=int, delimiter=",")
     return(file_data)
 # Load data of training
 def load_data(x_name,y_name):
@@ -30,10 +30,19 @@ def ann_pso(x,y,param):
     hidden_activation = param[0]
     hidden_nodes_number = param[1]
     particle_number = param[2]
-    iteration_number = param[3]
-    space = pso.iniSwarm(iteration_number,particle_number)
-    
-    return(w1,w2)
+    max_iter = param[3]
+    pg_mse = 10^8
+    S, P, Pg, V = pso.iniSwarm( L = hidden_nodes_number , m = x.shape[1] ,K = len(y[0]), n_p = particle_number )
+
+    for i in range(max_iter):
+        
+        Fits = pso.Fitness_mse(y, x, S, particle_number,hidden_nodes_number)
+        P, Pg, pg_mse = pso.upd_pFitness(P, Pg, Fits,x,y,S,hidden_nodes_number,particle_number,pg_mse)
+        alpha = pso.calc_alpha(i,max_iter)
+        V = pso.upd_veloc(S,P,Pg,V,alpha)
+        S = S + V
+    return(w,v)
+
 # Training:ANN-PSO and  ANN-BP
 def train_ann(x,y,param_PSO,param_bp):
     w1,w2 = ann_pso(x,y,param_PSO)
@@ -44,7 +53,7 @@ def train_ann(x,y,param_PSO,param_bp):
 def main():
     param_PSO = load_config("cnf_ann_pso.csv")            
     param_bp = load_config("cnf_ann_bp.csv")            
-    xe,ye = load_data()   
+    xe,ye = load_data("xtrn.csv","ytrn.csv")   
     w1,w2 = train_ann(xe,ye,param_PSO,param_bp)             
     save_w(w1,w2)
        
